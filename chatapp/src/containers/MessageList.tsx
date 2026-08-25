fc0b3881231244fe832dd5e929778c93b022c8af
Noah-Ian <noahian140@gmail.com>
2026-08-25 23:38:07 +0300

    added import line

--- chatapp/src/containers/MessageList.tsx
+++ chatapp/src/containers/MessageList.tsx
@@ -1,6 +1,6 @@
 import type { RootState } from "../types/index";
 import { legacy_connect as connect } from "react-redux";
-import MessageListComponent  from "../components/Messagelist";
+import MessageListComponent  from "../components/MessageList";
 
 
 const mapStateToProps = (state: RootState)=>({
